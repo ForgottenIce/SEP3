cbc1991b1910116192e6bbe0ad8e4b40086a6244
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-10-12 10:40:42 +0200

    added the required scope service to connect blazor to the API

--- SEP3CSharp/BlazorClient/Program.cs
+++ SEP3CSharp/BlazorClient/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7021") }); //restAPI address may vary, check it's right
 
 await builder.Build().RunAsync();
