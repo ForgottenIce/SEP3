66a9cc5e37d6aba85e9854139b5a1a6500cbfcc5
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-10-26 13:13:32 +0200

    csharp grpc init, with ping proto client

--- SEP3CSharp/gRPC/Class1.cs
+++ SEP3CSharp/gRPC/Class1.cs
@@ -1,5 +1,14 @@
-﻿namespace gRPC;
+﻿using Grpc.Net.Client;
+using gRPC;
 
+namespace gRPC;
 public class Class1
 {
+    public async Task<PingResponse> PingServerAsync()
+    {
+        using var channel = GrpcChannel.ForAddress("");
+        var client = new Ping.PingClient(channel);
+        var reply = await client.pingAsync(new PingRequest { DateTime = 2 });
+        return reply;
+    } 
 }
