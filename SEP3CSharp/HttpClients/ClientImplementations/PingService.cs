c4db640e040875e4afad1d90a8cfaed027f034a7
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-11-09 11:56:01 +0100

    moved ping http impl
    moved ping http impl from component to their own classes in the httpcliens project

---
+++ SEP3CSharp/HttpClients/ClientImplementations/PingService.cs
@@ -0,0 +1,26 @@
+﻿using HttpClients.ClientInterfaces;
+using System.Text.Json;
+
+namespace HttpClients.ClientImplementations;
+public class PingService : IPingService {
+    private readonly HttpClient client;
+
+    public PingService(HttpClient client) { 
+       this.client = client;
+    }
+    public async Task<long[]?> PingAsync() {
+        HttpResponseMessage response = await client.GetAsync("/Ping");
+        string result = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode) {
+            throw new Exception(result); //TODO implement more telling exception
+        }
+
+        long[]? dateTimes = JsonSerializer.Deserialize<long[]>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        return dateTimes;
+    }
+}
