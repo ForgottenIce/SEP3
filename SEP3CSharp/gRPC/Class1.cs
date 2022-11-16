b1741a58b81795c788c164f349becfc5a575abbb
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-11-16 14:26:24 +0100

    fixed grpc ping client
    made grpc ping client impl stop using https and TLS, ping can now again contact java grpc

--- SEP3CSharp/gRPC/Class1.cs
+++ SEP3CSharp/gRPC/Class1.cs
@@ -5,6 +5,9 @@ public static class Class1
 {
     public static async Task<PingResponse> PingServerAsync()
     {
+        AppContext.SetSwitch(
+            "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+        
         using var channel = GrpcChannel.ForAddress("http://localhost:9090");
         var client = new Ping.PingClient(channel);
         var reply = await client.pingAsync(new PingRequest { DateTime = DateTimeOffset.Now.ToUnixTimeMilliseconds() });
