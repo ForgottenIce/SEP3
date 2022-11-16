8d2d51d192dd4cf8daa8a1f99d83a450fb5e026c
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-11-16 09:58:51 +0100

    converted java project to spring boot

--- SEP3Java/src/main/java/PingServiceImpl.java
+++ SEP3Java/src/main/java/PingServiceImpl.java
@@ -2,9 +2,10 @@
 import grpc.PingGrpc;
 import grpc.PingOuterClass;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
 import java.time.Instant;
-
+@GrpcService
 public class PingServiceImpl extends PingGrpc.PingImplBase {
   @Override public void ping(PingOuterClass.PingRequest request,
       StreamObserver<PingOuterClass.PingResponse> responseObserver) {
---
+++ SEP3Java/src/test/java/groupid/sep3java/Sep3JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package groupid.sep3java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep3JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
