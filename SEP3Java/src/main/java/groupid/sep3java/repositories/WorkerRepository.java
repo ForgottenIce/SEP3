c5b8e471fcb4fef32e5f0cdd641205d4b7e794a5
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-11-16 11:18:33 +0100

    beginning of database setup
    beginning of database setup with spring data, current repository still needs entity object, but is connected to the global elephantsql database

---
+++ SEP3Java/src/main/java/groupid/sep3java/repositories/WorkerRepository.java
@@ -0,0 +1,6 @@
+package groupid.sep3java.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Object ,Long> { // TODO add model entity
+}
