6837c66fa6916f1668a5c2785b8a18558097a6cc
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2022-11-28 14:25:36 +0100

    Web API Controllers & logic
    Basic controller endpoints and logic

---
+++ SEP3CSharp/Application/Logic/ProductLogic.cs
@@ -0,0 +1,23 @@
+﻿using Application.LogicInterfaces;
+using gRPC.ServiceInterfaces;
+using Shared.Dtos;
+using Shared.Models;
+
+namespace Application.Logic;
+public class ProductLogic : IProductLogic {
+    private readonly IProductService productService;
+
+    public ProductLogic(IProductService productService) {
+        this.productService = productService;
+    }
+
+    public async Task<Product> CreateProductAsync(ProductCreationDto dto) {
+        Product product = await productService.CreateProductAsync(dto);
+        return product;
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsAsync() {
+        IEnumerable<Product> products = await productService.GetProductsAsync();
+        return products;
+    }
+}
