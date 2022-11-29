788efce4107590d52a9c43e9e7f303d9b01836d9
sajit <sajithijan1245@gmail.com>
2022-11-29 09:53:57 +0100

    Error in error

---
+++ SEP3CSharp/Shared/Exception/InvalidAgeException.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Exception;
+
+public class InvalidAgeException : System.Exception
+{
+    public InvalidAgeException() : base("Age is invalid")
+    {
+    }
+
+    public InvalidAgeException(string? message) : base(message)
+    {
+    }
+
+    public InvalidAgeException(string message, string? paramName, System.Exception? innerException) : base(message, paramName, innerException)
+    {
+    }
+}
