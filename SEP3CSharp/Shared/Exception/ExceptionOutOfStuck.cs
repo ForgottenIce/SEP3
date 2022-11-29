f5e59e9b88ef6bc514a3c03c1150dada6f957e08
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2022-11-29 09:48:58 +0100

    Exceptions add

---
+++ SEP3CSharp/Shared/Exception/ExceptionOutOfStuck.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Exception;
+
+public class ExceptionOutOfStuck: ArgumentException {
+    
+    public ExceptionOutOfStuck() : base("Out of stuck of" /* + Item*/) {
+    }
+
+    public ExceptionOutOfStuck(string? message) : base(message) {
+    }
+
+    public ExceptionOutOfStuck(string? message, string? paramName, System.Exception? innerException) : base(message, paramName, innerException) {
+    }   
+}
---
+++ SEP3CSharp/Shared/Exception/WrongPasswordException.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.Exception;
+
+public class WrongPasswordException: ArgumentException {
+    
+    public WrongPasswordException() : base("Password dose not match the user") {
+    }
+
+    public WrongPasswordException(string? message) : base(message) {
+    }
+
+    public WrongPasswordException(string? message, string? paramName, System.Exception? innerException) : base(message, paramName, innerException) {
+    }
+
+    
+}
