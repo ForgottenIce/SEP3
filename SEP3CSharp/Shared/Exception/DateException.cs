6e59e1f30f39a48b9f386bcc8b863625f6a29fab
sajit <sajithijan1245@gmail.com>
2022-11-29 10:40:26 +0100

    Error in error

---
+++ SEP3CSharp/Shared/Exception/DateException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Shared.Exception;
+
+public class DateException : NotSupportedException
+{
+    public DateException() : base("Date is not correct")
+    {
+    }
+
+    protected DateException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public DateException(string? message) : base(message)
+    {
+    }
+
+    public DateException(string? message, string? paramName, System.Exception? innerException) : base(message, paramName, innerException)
+    {
+    }
+}
---
+++ SEP3CSharp/Shared/Exception/TimeException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Shared.Exception;
+
+public class TimeException : NotSupportedException
+{
+    public TimeException() : base("Time is not correct")
+    {
+    }
+
+    protected TimeException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public TimeException(string? message) : base(message)
+    {
+    }
+
+    public TimeException(string? message, string? paramName, System.Exception? innerException) : base(message, paramName, innerException)  
+    {
+    }
+}
