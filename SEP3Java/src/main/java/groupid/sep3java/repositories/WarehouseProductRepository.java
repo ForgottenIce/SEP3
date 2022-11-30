ab3ad6f6450c37989e19236868f606303ab82f01
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-11-30 14:47:12 +0100

    chanced WarehouseProductRepository
    chanced WarehouseProductRepository to have its key be the composite key

--- SEP3Java/src/main/java/groupid/sep3java/repositories/WarehouseProductRepository.java
+++ SEP3Java/src/main/java/groupid/sep3java/repositories/WarehouseProductRepository.java
@@ -1,7 +1,8 @@
 package groupid.sep3java.repositories;
 
 import groupid.sep3java.models.WarehouseProduct;
+import groupid.sep3java.models.WarehouseProductID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WarehouseProductRepository extends JpaRepository<WarehouseProduct, Long> {
+public interface WarehouseProductRepository extends JpaRepository<WarehouseProduct, WarehouseProductID> {
 }
