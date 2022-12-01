54daf7f29a3c186798a8ff3a7cdbe9c7014e3359
aljanabee <aljanabee@hotmail.com>
2022-12-01 10:41:30 +0100

    warehouseproductlogic

--- SEP3CSharp/Application/Logic/WarehouseProductLogic.cs
+++ SEP3CSharp/Application/Logic/WarehouseProductLogic.cs
@@ -1,6 +1,28 @@
-﻿namespace Application.Logic;
+﻿using Application.LogicInterfaces;
+using gRPC.ServiceInterfaces;
+using Shared.Models;
 
-public class WarehouseProductLogic
+namespace Application.Logic;
+
+public class WarehouseProductLogic : IWarehouseProductLogic
+{
+    private readonly IWarehouseProductService _warehouseProductService;
+
+    public WarehouseProductLogic(IWarehouseProductServise warehouseProductServise)
+    {
+        this._warehouseProductService = warehouseProductServise;
+    }
+
+    public Task<WarehouseProduct> GetWarehouseProductByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<WarehouseProduct>> GetWarehouseProductAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
 {
     
 }
