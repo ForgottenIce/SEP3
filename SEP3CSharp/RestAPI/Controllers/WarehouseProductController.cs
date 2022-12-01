a52c13d688e68a5f8972e0228e2b3a020ac8aa46
aljanabee <aljanabee@hotmail.com>
2022-12-01 09:59:38 +0100

    WarehouseProductcontroller

---
+++ SEP3CSharp/RestAPI/Controllers/WarehouseProductController.cs
@@ -0,0 +1,15 @@
+﻿using Application.LogicInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dtos;
+using Shared.Models;
+
+
+namespace RestAPI.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+
+public class WarehouseProductController : ControllerBase {
+    
+}
