1f392731d2a6f608f03a3f5bc3eee4545f526068
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2022-12-01 10:04:32 +0100

    IWarehouseProductService and WarehouseProductDto created

---
+++ SEP3CSharp/HttpClients/ClientIntefaces/IWarehouseProductService.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Dtos;
+using Shared.Models;
+
+namespace HttpClients.ClientIntefaces;
+
+public interface IWarehouseProductService
+{
+    Task<WarehouseProduct> CreateWarehouseProductAsync(WarehouseProductDto dto);
+    Task<IEnumerable<WarehouseProduct>> GetWarehouseProductsAsync();
+}   
---
+++ SEP3CSharp/Shared/Dtos/WarehouseProductDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared.Dtos;
+
+public class WarehouseProductDto
+{
+    public long ProductId { get;}
+    public long WarehouseId { get;}
+    public int Quantity { get;}
+    public int MinimumQuantity { get;}
+    public string WarehousePosition { get;}
+
+    public WarehouseProductDto(long productId, long warehouseId, int quantity, int minimumQuantity, string warehousePosition)
+    {
+        ProductId = productId;
+        WarehouseId = warehouseId;
+        Quantity = quantity;
+        MinimumQuantity = minimumQuantity;
+        WarehousePosition = warehousePosition;
+    }
+}
