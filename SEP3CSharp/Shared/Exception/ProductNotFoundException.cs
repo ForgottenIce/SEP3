533f5ebacdeb93244f28f626c6eb2d3ce57cd07e
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2022-12-01 10:26:34 +0100

    More Exceptions!!

---
+++ SEP3CSharp/Shared/Exception/ProductNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Exception;
+
+public class ProductNotFoundException : ArgumentException {
+    
+    public ProductNotFoundException() : base("No product was found.") {
+    }
+
+    public ProductNotFoundException(string? message) : base(message) {
+    }
+
+    public ProductNotFoundException(string? message, string? paramName, System.Exception? innerException) : base(message, paramName, innerException) {
+    }
+}
