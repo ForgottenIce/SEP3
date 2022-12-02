ee74b6e2e5cac50c0eb8ade2d292508079c1eaad
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-12-02 10:01:08 +0100

    added missing methode from WarehouseProductService interface

--- SEP3CSharp/gRPC/ServiceInterfaces/IWarehouseProductService.cs
+++ SEP3CSharp/gRPC/ServiceInterfaces/IWarehouseProductService.cs
@@ -6,5 +6,6 @@ namespace gRPC.ServiceInterfaces;
 public interface IWarehouseProductService
 {
     Task<WarehouseProduct> CreateWarehouseProductAsync(WarehouseProductCreationDto dto);
+    Task<WarehouseProduct> GetWarehouseProductByIdAsync(int id);
     Task<IEnumerable<WarehouseProduct>> GetWarehouseProductsAsync();
 }   
