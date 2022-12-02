989b2dd5097a210d892ad2ea75a5066ec21a9332
aljanabee <aljanabee@hotmail.com>
2022-12-02 12:08:22 +0100

    customerLogic og IcustomerLogic

---
+++ SEP3CSharp/Application/Logic/CustomerLogic.cs
@@ -0,0 +1,27 @@
+﻿using Application.LogicInterfaces;
+using gRPC.ServiceInterfaces;
+using Shared.Models;
+
+namespace Application.Logic;
+
+
+public class CustomerLogic : ICustomerLogic
+{
+    private readonly ICustomerLogic _customerService;
+
+    public CustomerLogic(ICreateCustomerService cutomerService)
+    {
+        _customerService = cutomerService;
+    }
+
+    public async Task<IEnumerable<Customer>> GetCustomerAsync()
+    {
+        IEnumerable<Customer> customers = await _customerService.GetCustomerAsync();
+        return customers;
+    }
+
+    public Task<Customer> GetCustomerByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+}
