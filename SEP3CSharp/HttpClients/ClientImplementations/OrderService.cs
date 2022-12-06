caf7e43c71f32956eb8929f8dd577d8d872f42de
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2022-12-06 14:44:26 +0100

    OrderService HttpClient
    Added an OrderService HttpClient

---
+++ SEP3CSharp/HttpClients/ClientImplementations/OrderService.cs
@@ -0,0 +1,55 @@
+﻿using HttpClients.ClientIntefaces;
+using Shared.Dtos;
+using Shared.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace HttpClients.ClientImplementations;
+
+public class OrderService : IOrderService {
+    private readonly HttpClient _httpClient;
+
+    public OrderService(HttpClient httpClient) {
+        _httpClient = httpClient;
+    }
+
+    public async Task<Order> CreateOrderAsync(OrderCreationDto dto) {
+        HttpResponseMessage response = await _httpClient.PostAsJsonAsync("/order", dto);
+        string content = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode) {
+            string result = await response.Content.ReadAsStringAsync();
+            throw new Exception(result);
+        }
+
+        Order order = JsonSerializer.Deserialize<Order>(content, new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return order;
+    }
+
+    public async Task<Order> GetOrderByIdAsync(long id) {
+        HttpResponseMessage response = await _httpClient.GetAsync("/order");
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode) {
+            throw new Exception(content);
+        }
+        Order order = JsonSerializer.Deserialize<Order>(content, new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return order;
+    }
+
+    public async Task<IEnumerable<Order>> GetOrdersAsync() {
+        HttpResponseMessage response = await _httpClient.GetAsync("/order");
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode) {
+            throw new Exception(content);
+        }
+        IEnumerable<Order> orders = JsonSerializer.Deserialize<IEnumerable<Order>>(content, new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return orders;
+    }
+}
