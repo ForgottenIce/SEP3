4d2c872a0848c1ce6805c2125b73c1c8b79ff9ba
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2022-12-07 11:54:34 +0100

    Warehouse reference in Order
    - Added a reference to warehouse in order so it's possible to find all orders that is associated with a warehouse
    - Fixed an error where orderedProducts was null on the received Order object

--- SEP3CSharp/Shared/Dtos/OrderCreationDto.cs
+++ SEP3CSharp/Shared/Dtos/OrderCreationDto.cs
@@ -3,6 +3,7 @@
 namespace Shared.Dtos;
 public class OrderCreationDto {
     public long CustomerId { get; set; }
+    public long WarehouseId { get; set; }
     public DateTime? DateTimeOrdered { get; set; }
     public bool IsPacked { get; set; }
     public DateTime? DateTimeSent { get; set; }
